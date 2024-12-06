f454ec3d8ff83e29d8e71b8d9e43d0fddf499577
iUsup <yusub1988@gmail.com>
2024-12-06 17:31:37 +0300

    fix: Select method works

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -1,49 +1,51 @@
 package hiber.dao;
 
 import hiber.model.User;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    private final SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    public UserDaoImp(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
-   @Override
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Transactional
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsersByCarModel(String model) {
-      String hql = "FROM User WHERE car.model LIKE :model";
-      Session session =sessionFactory.getCurrentSession();
-      Query<User> query = session.createQuery(hql);
-      query.setParameter("model", "%" + model + "%");
-      return query.getResultList();
-   }
+    @Transactional
+    @Override
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsersByCarSeries(int series) {
-      String hql = "FROM User WHERE car.series LIKE :series";
-      Session session =sessionFactory.getCurrentSession();
-      Query<User> query = session.createQuery(hql);
-      query.setParameter("series", series);
-      return query.getResultList();
-   }
+    @Transactional
+    @Override
+    public List<User> listUsersByCarModel(String model) {
+        String hql = "FROM User WHERE car.model LIKE :model";
+        Query<User> query = sessionFactory.getCurrentSession().createQuery(hql, User.class);
+        query.setParameter("model", "%" + model + "%");
+        return query.getResultList();
+    }
+
+    @Transactional
+    @Override
+    public List<User> listUsersByCarSeries(int series) {
+        String hql = "FROM User WHERE car.series = :series";
+        Query<User> query = sessionFactory.getCurrentSession().createQuery(hql, User.class);
+        query.setParameter("series", series);
+        return query.getResultList();
+    }
 }
